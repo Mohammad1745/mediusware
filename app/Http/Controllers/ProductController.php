ec15a9d01ce1fd278934dc71b63d47cf3787901e
ali <mdali2016.227@gmail.com>
2022-07-17 13:31:35 +0600

    [Add] implemented product service

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,28 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Services\ProductService;
 use App\Models\Product;
 use App\Models\ProductVariant;
 use App\Models\ProductVariantPrice;
 use App\Models\Variant;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class ProductController extends Controller
 {
     /**
+     * @var ProductService
+     */
+    private $service;
+
+    /**
+     * @param ProductService $service
+     */
+    function __construct(ProductService $service)
+    {
+        $this->service = $service;
+    }
+
+    /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Http\Response|\Illuminate\View\View
+     * @return Application|Factory|RedirectResponse|View
      */
     public function index()
     {
-        return view('products.index');
+        $response = $this->service->getProducts();
+        return $response['success'] ?
+            view('products.index', ['products'=>$response['data']])
+            : redirect()->back();
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Http\Response|\Illuminate\View\View
+     * @return Application|Factory|\Illuminate\Http\Response|View
      */
     public function create()
     {
--- app/Http/Services/ProductService.php
+++ app/Http/Services/ProductService.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Services;
 
+use App\Models\Product;
+
 class ProductService
 {
     use ResponseService;
 
+    public function getProducts () {
+        try {
+            $products = Product::all();
+//            dd ($products);
 
+            return $this->response($products->toArray())->success();
+
+        } catch (\Exception $exception) {
+            dd($exception);
+
+            return $this->response()->error($exception->getMessage());
+        }
+    }
 }
